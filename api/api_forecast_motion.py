d8992d52ca7ab33cc5c7166c57dd6656dda24b39
Kaique <kajus9138@gmail.com>
2024-11-14 09:54:32 -0300

    Primeiro commit

---
+++ api/api_forecast_motion.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+# Definir URL e credenciais
+#first-order-motion
+url = 'https://api.mooring.technomar.com.br/rest/database/first-order-motion-imu/range/mmsi/311000132?startDate=2024-01-27T23:30:00.000Z&endDate=2024-10-31T23:30:00.000Z'
+
+usr = 'service.ukc@technomar.com.br'
+pss = 'ukc@2023'
+
+# Obter Token
+token_url = 'https://api.accounts.technomar.com.br/rest/authenticator/token/password-grant'
+token_data = {
+    'grant_type': 'password',
+    'client_id': '62ad5c760d3d743765242ac9',
+    'client_secret': 'TZ0BZCNQ5rR2U966Wocit0TgI0FkReuVV1yRygsNLKg=',
+    'username': usr,
+    'password': pss,
+    'scope': '',
+    'redirect_uri': ''
+}
+
+# Enviar solicitação de token
+response = requests.post(token_url, json=token_data)
+token = response.json().get('access_token')
+
+# Verificar se o token foi obtido
+if not token:
+    raise Exception("Erro ao obter o token de autenticação")
+
+# Definir cabeçalhos da requisição
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/json'
+}
+
+# Enviar requisição à API
+response = requests.get(url, headers=headers)
+
+# Verificar se a solicitação foi bem-sucedida
+if response.status_code == 200:
+    forecast = response.json()
+    
+    # Salvar o JSON em um arquivo
+    with open('forecast_2.json', 'w') as file:
+        json.dump(forecast, file, indent=2)
+    print("Arquivo 'forecast.json' salvo com sucesso.")
+else:
+    raise Exception(f"Erro na requisição: {response.status_code}")
---
+++ dataset_generator.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import json
+
+
+caminho_forecast_ambiental = r'C:\Users\ksilva\Documents\Motion_Forecast\dados/forecast_waves_20240127_20241031.json'
+caminho_motion = r'C:\Users\ksilva\Documents\Motion_Forecast\dados/forecast_motion_20240127_20241031.json'
+
+# Carregando dados ambientais***********************************
+with open(caminho_forecast_ambiental, 'r') as file:
+    data = json.load(file)
+
+forecast_wave_data = data["forecastWave"]
+df_forecast_wave = pd.DataFrame(forecast_wave_data)
+df_forecast_wave = df_forecast_wave[['timestamp', 'wave_height', 'wave_height_max', 'peak_period', 'wave_height_swell',
+                                    'wave_height_sea','wave_dir_swell', 'wave_dir_sea','peak_period_swell', 'peak_period_sea'
+                                    ]]
+df_forecast_wave['timestamp'] = pd.to_datetime(df_forecast_wave['timestamp'])
+df_forecast_wave.set_index('timestamp', inplace=True)
+
+# Carregando dados de movimento***********************************
+with open(caminho_motion, 'r') as file:
+    data = json.load(file)
+
+df = pd.DataFrame(data)
+df_motion = df[['timestamp','amp_roll', 'amp_pitch', 'amp_heave']]
+df_motion['timestamp'] = pd.to_datetime(df_motion['timestamp'])
+df_motion.set_index('timestamp', inplace=True)
+
+df_motion.rename(columns={'amp_roll': 'roll', 'amp_pitch': 'pitch', 'amp_heave':'heave'}, inplace=True)
+df_motion_3h = df_motion.resample('3H').median()
+
+df_motion_3h.index.name = 'timestamp'
+df_total = pd.merge(df_motion_3h, df_forecast_wave, left_index=True, right_index=True, how='inner')
+df_total.drop(['wave_height', 'wave_height_max', 'peak_period'], inplace=True, axis=1)
+
+df_total.to_csv('df_envmotion.csv')
+
+
